2b19044c1e681a22242919b4f388b34eaf6f0924
Alexander Grygoriev <truedevelop@yahoo.com>
2022-02-20 17:21:39 +0200

    some learning REact

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import {Component, StrictMode} from 'react';
+import './App.css';
+
+const HelloW = ()=>{
+  return <h2>Hello World!</h2>
+};
+
+class Field extends Component{
+  render(){
+    const holder ='Enter Here'
+    const styleComp ={
+      width:'400px'
+    };
+
+    return <input placeholder={holder} 
+      type={'text'} 
+      style={styleComp}/>
+  }
+}
+
+function Btn(){
+  const text = 'Log In'
+  const logged = false;
+  
+  return <button>{logged?'Enter': text}</button>
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <StrictMode>
+        <HelloW/>
+      </StrictMode>
+      <Field/>
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>,
+  document.querySelector('#root')
+  );
+
+
+
+
+
+
+
