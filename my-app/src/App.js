987274fe71b97c1c5249f49d8604681a9d6fe994
Alexander Grygoriev <truedevelop@yahoo.com>
2022-03-03 07:56:35 +0200

    introduce to react repeating

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,58 +3,41 @@ import {Component} from 'react';
 import './App.css';
 
 
-class WhoAmI extends Component{
-  constructor(props){
-    super(props);
-    this.state= {
-      years: 27,
-      text: 'myButonToADD'
-    }
-      
-    }
+const Header = () => {
+  return <h2>Hello React!</h2>
+}
 
-    nextYear=()=>{
-      this.setState (state=> ({
-        years : this.state.years+1
-      }))
-    }
 
-    commitInputChanges = (e,color)=>{
-      console.log(color)
-      this.setState({
-        position:e.target.value
-      })
-    }
+
+class Field extends Component {
   render(){
-    const {name, surname, link} = this.props;
-    const {position, years} = this.state;
-
-    console.log(this);
-
-    return(
-      <div>
-        <button onClick={this.nextYear}>{this.state.text}</button>
-        <h2>My name is {name}, surname {surname} , 
-        age {years}, 
-        position-{position}</h2>
-        <a href={link}>my link</a>
-        <form>
-          <span>Введите должность</span>
-          <input type="text" onChange={(e)=>this.commitInputChanges(e,"some color")} />
-        </form>
-      </div>
-    );
+    const holder = 'Enter here'
+    const styleField = {
+      backgroundColor: 'yellow',
+      width: '300px',
+      height: '50px'
+    };
+ 
+return <input type="text" placeholder={holder} style={styleField}/>
   }
 }
 
+function Btn() {
+  const text = "Log in";
+  const logged = false;
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name= 'JOhn' surname = "Petrov" link="yandex.ru"/>
-      <WhoAmI name= 'Alex' surname = "Sidorov" link="facebook.com"/>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
+
 
 export default App;
