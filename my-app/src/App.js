01d0f3cb8fd65bf586ff0287a610036890a493a7
Alexander Grygoriev <truedevelop@yahoo.com>
2022-03-26 10:44:53 +0200

    Portal in Ract learning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 
 import React, {Component} from 'react';
+import ReactDom from 'react-dom';
 import {Container} from 'react-bootstrap';
 import './App.css';
 import BootstrapTest from './bootstrapTest';
@@ -12,36 +13,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Form extends Component {
 
-setInputRef = (elem) => {
-  this.myRef = elem;
-}
-
-
-  focusIT = () => {
-    if(this.myRef) {
-      this.myRef.focus();
-    }
+  state = {
+    advOpen: false
   }
 
+  componentDidMount() {
+    setTimeout(this.handleClick, 3000);
+  }
+
+  handleClick = () => {
+    this.setState(({advOpen}) => ({
+      advOpen: !advOpen
+    }))
+  }
 
   render() {
     return(
       <Container>
-        <form className='w-50 border mt-5 p-3 m-auto'>
+        <form onClick={this.handleClick}
+        className='w-50 border mt-5 p-3 m-auto'
+        style={{'position':'relative',
+                'overflow':'hidden'}}>
           <div className='mb-3'>
             <label htmlFor='exampleFormControlInput1' className="form-label">Email Address</label>
-            <input ref={this.setInputRef} type="email" className='form-control' id='exampleFormControlInput1'placeholder='name@example.com' />
+            <input type="email" className='form-control' id='exampleFormControlInput1'placeholder='name@example.com' />
           </div>
           <div className='mb-3'>
             <label htmlFor="exampleFormControlTextArea1" className='form-label'>Example Text-Area</label>
             <textarea 
-            onClick={this.focusIT}
             className='form-control' id='exampleFormControlTextArea1' rows='3'></textarea>
           </div>
+          {
+            this.state.advOpen ? 
+            <Portal>
+              <Msg/>
+            </Portal> : null
+          }
+
         </form>
       </Container>
     )
   }
+}
+
+const Portal = (props) => {
+  const node = document.createElement('div');
+  document.body.appendChild(node);
+  return ReactDom.createPortal(props.children, node);
+}
+
+const Msg = () => {
+  return(
+    <div
+        style={{'width': '500px',
+                'height': '150px',
+                'backgroundColor': 'red',
+                'position': 'absolute',
+                'right': '0',
+                'bottom': '0'}
+
+    }>
+      Hello
+    </div>
+  )
 }
 
 function App() {
