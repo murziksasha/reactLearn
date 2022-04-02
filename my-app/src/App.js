2ec9d1a0e88e9ca9aebe638deb0b42074fe5dbad
Alexander Grygoriev <truedevelop@yahoo.com>
2022-04-02 09:27:15 +0300

    start template form book

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,11 +15,36 @@ class LightningCounter extends Component {
   }
 
   timerTick() {
-    this.setState(()=>{
+    this.setState(()=>({
 
         strikes: this.state.strikes + 100
 
-    })
+    }))
+  }
+
+  render(){
+    return (
+      <h1>Hello!</h1>
+    )
+  }
+}
+
+class LightningCounterDisplay extends Component {
+  render(){
+    const divStyle = {
+      width: 250,
+      textAlign: 'center',
+      backgroundColor: 'black',
+      padding: 40,
+      fontFamily: 'sans-serif',
+      color: '#999',
+      borderRadius: 10
+    };
+    return(
+      <div style={divStyle}>
+        <LightningCounter/>
+      </div>
+    )
   }
 }
 
@@ -30,7 +55,7 @@ function App() {
   return (
 
     <div className='App'>
-
+      <LightningCounterDisplay/>
 
     </div>
 
