15934f97bf08c4b6670929d5a6572f4ba1410b65
Sasha Grygoriev <truedevelop@yahoo.com>
2022-04-10 18:53:23 +0300

    hook setState Petrichenko

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,43 @@
 
-import React, {useState, useEffect} from 'react';
+import {useState} from 'react';
 import './App.css';
 
+import {Container} from 'react-bootstrap';
 
 
-function Example () {
-  const [count, setCount] = useState(0);
+const Slider = (props) => {
 
-  useEffect(() => {
-    document.title = `Вы ${count} раз`;
-  });
+  const [slide, setSlide] = useState(() => 0);
+  const [autoplay, setAutoplay] = useState(() => false);
 
-  const minus = () => {
-    (
-      setCount(count -1)
-    )
+  const changeSlide = (i) => {
+    setSlide(slide => slide + i)
   }
 
-  return (
-    <>
-        <p>You click {count} ways</p>
-      <button style={{'marginRight': '30px'}} 
-      onClick={() => setCount(count+1)}
-      >Click me</button>
-            <button onClick={minus}
-      >minus click</button>
-    </>
+  const toggleAutoplay = () => {
+    setAutoplay(autoplay => !autoplay)
+  }
 
-  )
+    return(
+
+      <Container>
+        <div className="slider w-50 m-auto">
+          <img src="https://media.istockphoto.com/photos/planet-earth-with-some-clouds-americas-view-picture-id186019678?k=20&m=186019678&s=612x612&w=0&h=E9ZFggtDpeOkSlOBg8QgdaOoq5xsOunmBCNMGc2VNFg=" alt="Earth Planet" style={{'width':100}} />
+          <div className="text-center mt-5">Active slide {slide} <br/>{autoplay?'auto':null}</div>
+          <div className="buttons mt-3">
+          <button
+              className='btn btn-primary me-2'
+              onClick={() => changeSlide(-1)}>-1</button>            
+              <button
+              className='btn btn-primary me-2'
+              onClick={() => changeSlide(1)}>+1</button>            
+              <button
+              className='btn btn-primary me-2'
+              onClick={()=>toggleAutoplay()}>Toggle auto play</button>
+          </div>
+        </div>
+        </Container>
+    )
 }
 
 function App() {
@@ -36,7 +46,7 @@ function App() {
   return (
 
     <div className='App'>
-      <Example/>
+      <Slider/>
 
     </div>
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 
-import React, {StrictMode, Component} from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
