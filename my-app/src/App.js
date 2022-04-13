0caa0a82717cde2e3e0c5ff7657c324572f51b3d
Alexander Grygoriev <truedevelop@yahoo.com>
2022-04-13 14:51:14 +0300

    useRef react learning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,122 +1,46 @@
 
-import {useState, useEffect, useCallback, useMemo} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import './App.css';
 
 import {Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const countTotal = (num) => {
-  console.log('counting...');
-  return num + 10;
-}
 
-const Slider = (props) => {
-
-  const [slide, setSlide] = useState(() => 0);
-  const [autoplay, setAutoplay] = useState(() => false);
-
-  const getSomeImages = useCallback(() => {
-    console.log('fetching');
-    return [
-      'https://media.istockphoto.com/photos/planet-earth-with-some-clouds-americas-view-picture-id186019678?k=20&m=186019678&s=612x612&w=0&h=E9ZFggtDpeOkSlOBg8QgdaOoq5xsOunmBCNMGc2VNFg=',
-      'https://media.istockphoto.com/photos/planet-earth-with-some-clouds-americas-view-picture-id186019678?k=20&m=186019678&s=612x612&w=0&h=E9ZFggtDpeOkSlOBg8QgdaOoq5xsOunmBCNMGc2VNFg='
-    ]
-  },[]); 
-
-
-  function logging() {
-    console.log('log!');
-  }
-
-  useEffect(()=>{
-    console.log('effect');
-    document.title = `Slide ${slide}`;
-
-    window.addEventListener('click', logging);
-
-    return () => {
-      window.removeEventListener('click', logging);
-    }
-
-  },[slide])
-
-  useEffect(() => {
-    console.log('toggle state');
-
-  }, [autoplay])
-
-  const changeSlide = (i) => {
-    setSlide(slide => slide + i)
-  }
-
-  const toggleAutoplay = () => {
-    setAutoplay(autoplay => !autoplay)
-  }
-
-  const total = useMemo(()=> {
-    return countTotal(slide);
-  }, [slide]);
-
-  const style = useMemo(()=>({
-    
-      color: slide > 4 ? 'red': 'black'
-    
-  }),[slide])
-
-  useEffect(()=>{
-    console.log('styling....');
-  },[style]);
-
-    return (
-
-      <Container>
-        <div className="slider w-50 m-auto">
-
-        <Slide getSomeImages={getSomeImages}/> 
-
-        <div className="text-center mt-5"
-        style={style}>Total {total}</div>
-
-          <div className="text-center mt-5">Active slide {slide} <br/>{autoplay?'auto':null}</div>
-          <div className="buttons mt-3">
-          <button
-              className='btn btn-primary me-2'
-              onClick={() => changeSlide(-1)}>-1</button>            
-              <button
-              className='btn btn-primary me-2'
-              onClick={() => changeSlide(1)}>+1</button>            
-              <button
-              className='btn btn-primary me-2'
-              onClick={()=>toggleAutoplay()}>Toggle auto play</button>
-          </div>
-        </div>
-      </Container>
-    )
-}
-
-const Slide = ({getSomeImages}) => {
-  const [images, setImages]  =  useState([]);
-  useEffect(()=>{ 
-    setImages(getSomeImages())   
-
-  },[getSomeImages]) 
-  return (
-    <>
-    {images.map((url, i) =><img key ={i} src={url} alt="Earth Planet" 
-              className='d-block w-50'/>)}
-    </>
-  )
-}
 
 function App() {
 
-  const [slider, setSlider] = useState(()=>true);
+  const [text, setText] = useState('');
+
+
+  const myRef = useRef(1);
+
+  useEffect(()=>{
+    myRef.current = text;
+    
+  })
+
 
   return (
 
     <div className='App'>
-      <button onClick={() => setSlider(false) }>Click me</button>
-     {(slider)?<Slider/>:null}
+      <Container >
+        <form className='w-50 border mt-5 p-3 m-auto'>
+            <div className="mb-3">
+              <label htmlFor="exampleFromControlInput1">Email address</label>
+              <input onChange={(e)=>setText(e.target.value)}
+               type='email'
+              className='from-label' placeholder='name@forexample'
+              id="exampleFromControlInput1"/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFromControlTextArea1">Email address</label>
+              <textarea value={myRef.current}
+               id="exampleFromControlTextArea1" cols="30" rows="10"
+              className='form-control' rows='3'></textarea>
+            </div>
+
+        </form>
+      </Container>
     </div>
 
 
